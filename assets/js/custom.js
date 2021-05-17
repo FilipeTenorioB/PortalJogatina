48c227b0b3a4194e7c8d1f2cb63cece7153e4a94
FilipeTenorioB <50412204+FilipeTenorioB@users.noreply.github.com>
2021-05-17 10:39:22 -0300

    Adição do 'overflow-x'.
    CSS: Adição do 'overflow-x' na tag 'body' para corrigir o problema do scroll, na horizontal, que ultrapassava o conteúdo da página.
    
    Adição de função de responsividade ao banner no arquivo 'js'.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,5 +4,30 @@ $('.slider-BannerHomepage').slick({
   dots: false,
   autoplay: true,
   autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+]
 });
 });
